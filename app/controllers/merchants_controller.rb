4fd45a822e5a90b33e614ae4afdf4598ca908f53
Andrea Chen <andreachen.ac@gmail.com>
2017-10-17 16:34:14 -0700

    Add MerchantsController#show, corresponding tests, and template. Add private method to find specific merchant by merchant id

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,10 +1,22 @@
 class MerchantsController < ApplicationController
 
+before_action :find_id_by_params, except: [:index, :new, :create]
+
   def index
     @merchants = Merchant.all
   end
 
   def show
+    @merchant = Merchant.find_by(id: params[:id])
   end
-  
+
+  private
+
+  def find_id_by_params
+    @merchant = Merchant.find_by(id: params[:id])
+    unless @merchant
+      head :not_found
+    end
+  end
+
 end
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -3,15 +3,28 @@ require "test_helper"
 describe MerchantsController do
   describe "index" do
     it "succeeds when there are merchants" do
-    Merchant.count.must_be :>, 0, "No merchants in the test fixtures"
-    get merchants_path
-    must_respond_with :success
+      Merchant.count.must_be :>, 0, "No merchants in the test fixtures"
+      get merchants_path
+      must_respond_with :success
+    end
+
+    it "succeeds when there are no merchants" do
+      Merchant.destroy_all
+      get merchants_path
+      must_respond_with :success
+    end
   end
 
-  it "succeeds when there are no merchants" do
-    Merchant.destroy_all
-    get merchants_path
-    must_respond_with :success
-  end
+  describe "show" do
+    it "succeeds for an extant merchant ID" do
+      get merchant_path(Merchant.first)
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a invalid merchant ID" do
+      invalid_merchant_id = Merchant.last.id + 1
+      get merchant_path(invalid_merchant_id)
+      must_respond_with :not_found
+    end
   end
 end
