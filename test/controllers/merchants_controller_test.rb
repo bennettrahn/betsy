e0115402f46ae69b097b43954db8863c06efd67e
Andrea Chen <andreachen.ac@gmail.com>
2017-10-17 16:10:30 -0700

    Add tests for MerchantsController@index. Generate Merchant Model. Add test data for Merchants

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -2,6 +2,16 @@ require "test_helper"
 
 describe MerchantsController do
   describe "index" do
-    
+    it "succeeds when there are merchants" do
+    Merchant.count.must_be :>, 0, "No merchants in the test fixtures"
+    get merchants_path
+    must_respond_with :success
+  end
+
+  it "succeeds when there are no merchants" do
+    Merchant.destroy_all
+    get merchants_path
+    must_respond_with :success
+  end
   end
 end
