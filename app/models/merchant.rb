e912b8ed6af6e88a89ca449097066ab7bdd71f7f
Andrea Chen <andreachen.ac@gmail.com>
2017-10-17 20:36:39 -0700

    Add validations to Merchant model and corresponding tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,8 @@
 class Merchant < ApplicationRecord
 
-  validates :username, presence: {message: "The username must be present"}
+  validates :username, presence: {message: "Please enter a username"}
 
+  validates :email, presence: {message: "Please enter an email"}
+
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Please enter your email in this format: example@example.com"
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -5,7 +5,40 @@ describe Merchant do
     @valid_test_m = Merchant.new(username: "tricycle", email: "tricycle@tricycle.com" )
   end
 
-  it "must be valid" do
-    @valid_test_m.must_be :valid?
+  describe "validations" do
+    it "is valid when given valid merchant data" do
+      @valid_test_m.must_be :valid?
+    end
+
+    it "requires a username" do
+      merchant = Merchant.new(username: "", email: "email@email.com")
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :username
+    end
+
+    it "requires an email" do
+      merchant = Merchant.new(username: "username", email: "")
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :email
+    end
+
+    it "requires an email in a certain format" do
+      merchant = Merchant.new(username: "username", email: "tricycle")
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :email
+
+      merchant1 = Merchant.new(username: "username", email: "tricycle@")
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :email
+
+      merchant2 = Merchant.new(username: "username", email: "tricycle@tricycle")
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :email
+
+      merchant2 = Merchant.new(username: "username", email: "tricycle@tricycle.")
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :email
+      
+    end
   end
 end
