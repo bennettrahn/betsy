5dbdbccb38ca41647271076d1081a299374dcf14
Lauren <Laurenelee88@gmail.com>
2017-10-18 15:33:49 -0700

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,23 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @product = Product.find_by(id: params[:product_id])
   end
+
   def create
+    @product = Product.find_by(id: params[:product_id])
     @review = Review.new(
     review_params
     )
-  
     if @review.save
-      redirect_to products_path
+      redirect_to product_path(@product)
     else
       render :new, status: :bad_request
     end
+  end
+
+  def show
+    @review = Review.find_by[id: params[:id]]
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders
-  resources :products
+  resources :products do
+    resources :reviews, only: [:new, :create, :show]
+  end
   resources :merchants
-  resources :reviews, only: [:new, :create]
-  # nested route /product/:id/review 
+
+
 end
