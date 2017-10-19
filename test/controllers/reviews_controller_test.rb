19dad1db8cf2546f4d03c8d922d1144ab863b7ca
Lauren <Laurenelee88@gmail.com>
2017-10-18 21:46:48 -0700

    review tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -4,41 +4,54 @@ describe ReviewsController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+  let (:review) {reviews(:review1)}
 
-  before do
-    @review = Review.new
-  end
-
-  describe "relations" do
-    it "must respond to product" do
-      @review.must_respond_to :product
+  describe "new" do
+    it "creates a new review successfully" do
+      @product = Product.first.id
+      get new_product_review_path(@product)
+      must_respond_with :success
     end
   end
-  describe 'validations' do
-    it 'is valid' do
-      review = Review.new(rating: 3, product_id: Product.first.id)
-      review.must_be :valid?
-    end
 
-    it 'requires a rating' do
-      is_valid = @review.valid?
-      is_valid.must_equal false
-      @review.errors.messages.must_include :rating
-    end
+  describe "create" do
+    #  BOTH create tests need :product_id! HOW DO WE PASS IN?
 
-    it 'requires a ranking greater than 0' do
-      review = Review.new(rating: 0, product_id: 1)
-      is_valid = review.valid?
-      is_valid.must_equal false
-      review.errors.messages.must_include :rating
-    end
+  #   it "saves and redirects to product show page when the review data is valid" do
+  #    review_data = {
+  #      review: {
+  #        rating: 3,
+  #        text: "I loved it!",
+  #      }
+  #    }
+  #    @product =  Product.first.id
+  #    Review.new(review_data[:review]).must_be :valid?
+  #    start_review_count = Review.count
+  #
+  #    post new_product_review_path, params: product_data
+  #    must_respond_with :redirect
+  #    must_redirect_to product_path(@product)
+  #    Review.count.must_equal start_review_count + 1
+  #  end
 
-    it 'requires a ranking less than 5' do
-      review = Review.new(rating: 6, product_id: 1)
-      is_valid = review.valid?
-      is_valid.must_equal false
-      review.errors.messages.must_include :rating
-    end
+
+  #   it "renders a bad_request when the review data is invalid" do
+  #     bad_review = {
+  #       review: {
+  #         rating: "",
+  #         # no rating given!!
+  #       }
+  #     }
+  #     Review.new(bad_review[:review]).wont_be :valid?
+  #     start_review_count = Review.count
+  #     post new_product_review_path, params: bad_review
+  #
+  #     must_respond_with :bad_request
+  #     Review.count.must_equal start_review_count
+  #   end
+  #   # need to write test for when create is not allowed (not merchant_id)
   end
+
+
 
 end
