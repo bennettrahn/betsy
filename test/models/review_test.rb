e658912352f50b960c93a35c544cbb2132f90f6e
Lauren <Laurenelee88@gmail.com>
2017-10-18 19:07:22 -0700

    reviews controller tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,9 +1,10 @@
 require "test_helper"
 
 describe Review do
-  let(:review) { Review.new }
-
-  it "must be valid" do
-    value(review).must_be :valid?
-  end
+  # let(:review) { Review.new }
+  #
+  # it "must be valid" do
+  #   value(review).must_be :valid?
+  # end
+  
 end
