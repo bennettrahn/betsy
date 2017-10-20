95f6f28644194008ee42f9d4b8d13861dfa8a3e1
Bennett Rahn <rahnibennett@gmail.com>
2017-10-19 21:50:23 -0700

    started some functionality for checkout, not finished

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,4 +1,28 @@
 class OrderProductsController < ApplicationController
+  before_action :find_op_by_params_id, only: [:edit, :update]
+  def edit
+
+  end
+
+  def update
+    @op.update_attributes(order_params)
+
+    @product = Product.find_by(id: params[:product_id])
+    quantity = params[:quantity].to_i
+
+    if @product.check_inventory(quantity)
+      if save_and_flash(@order, edit:"updated")
+        redirect_to(order_path(@order))
+      else
+        render :edit, status: :bad_request
+      end
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Not enough #{@product.name.pluralize} in stock, please revise the quantity selected."
+      render "products/show", status: :bad_request
+    end
+  end
+
   def create
     assign_order
     @product = Product.find_by(id: params[:product_id])
@@ -13,8 +37,8 @@ class OrderProductsController < ApplicationController
         render "products/show", status: :bad_request
       end
     else
-      flash[:status] = :failure
-      flash[:message] = "Not enough #{@product.name.pluralize} in stock, please revise the quantity selected."
+      flash[:status].now = :failure
+      flash[:message].now = "Not enough #{@product.name.pluralize} in stock, please revise the quantity selected."
       render "products/show", status: :bad_request
     end
   end
@@ -28,6 +52,15 @@ private
       @order = order.id
       session[:cart] = order.id
     end
+  end
+
+  def order_params
+    return params.require(:order_product).permit(:quantity, :product_id)
+  end
+
+  def find_op_by_params_id
+    @op = OrderProduct.find_by(id: params[:id])
+    head :not_found unless @op
   end
 
 end
