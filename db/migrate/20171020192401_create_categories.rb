e2cff49582e5ee065b7ae50af16d6765074fb9ed
Lauren <Laurenelee88@gmail.com>
2017-10-20 12:31:00 -0700

    join table

---
+++ db/migrate/20171020192401_create_categories.rb
@@ -0,0 +1,9 @@
+class CreateCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.timestamps
+    end
+    create_join_table :products, :categories
+  end
+end
