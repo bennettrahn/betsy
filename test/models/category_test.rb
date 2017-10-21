d81823c7f7b21145b5fbdbcd6675b9e8e069dd2a
Andrea Chen <andreachen.ac@gmail.com>
2017-10-20 20:26:20 -0700

    Add tests for Category Model. All index page for Categories.

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,11 +1,7 @@
 require "test_helper"
 
 describe Category do
-  # let(:category) { Category.new }
-  #
-  # it "must be valid" do
-  #   value(category).must_be :valid?
-  # end
+
   let (:category) {categories(:book)}
 
   describe "relations" do
@@ -13,7 +9,23 @@ describe Category do
       category.must_respond_to :products
     end
   end
+
+  describe "validations" do
+    it "is valid when given valid category data" do
+      new_cat = Category.new(name: "record")
+      new_cat.must_be :valid?
+    end
+
+    it "requires a name" do
+      invalid_cat = Category.new(name: "")
+      invalid_cat.wont_be :valid?
+    end
+
+    it "requres a name that is unique" do
+      new_cat = Category.new(name: "record")
+      new_cat.save
+      new_cat_2 = Category.new(name: "record")
+      new_cat_2.wont_be :valid?
+    end
+  end
 end
-
-
-# unqiqueness and presence
