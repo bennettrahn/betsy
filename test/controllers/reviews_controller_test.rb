b00bb8c6fcdf100bd79bce042879365fd6be064a
Andrea Chen <andreachen.ac@gmail.com>
2017-10-22 10:02:58 -0700

    Rearrange ReviewsController tests to have clear distinction between logged in merchant and not logged in users. Still need to write 2 tests here - for #create. Currently when you select no rating, getting a id cannot be for nil class error

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -46,9 +46,9 @@ describe ReviewsController do
         Review.count.must_equal start_review_count + 1
       end
 
-
-    # it "renders a bad_request when the review data is invalid" do
+    # TODO: it "renders a bad_request when the review data is invalid" do
       # what if the star is left blank...
+
       it "doesn't allow a merchant to review their own product" do
         merchant = merchants(:anders)
         login(merchant)
@@ -69,6 +69,48 @@ describe ReviewsController do
         Review.count.must_equal start_review_count
       end
     end
-    # need to write test for when create is not allowed (not merchant_id)
+  end
+
+  #######NOT LOGGED IN USER##########
+  describe "not logged in user" do
+
+    describe "new" do
+      it "creates a new review successfully" do
+        @product = Product.first.id
+        get new_product_review_path(@product)
+        must_respond_with :success
+      end
+
+      it "returns not_found if given invalid product id" do
+        invalid_prod_id = Product.last.id + 1
+        get new_product_review_path(invalid_prod_id)
+        must_respond_with :not_found
+      end
+    end
+
+    describe "create" do
+      it "saves and redirects to product show page when the review data is valid" do
+
+        product =  Product.first
+        review_data = {
+          review: {
+          rating: 3,
+          text: "I loved it!",
+          product_id: product.id
+          }
+        }
+        puts review_data[:review][:product_id]
+        product.reviews.new(review_data[:review]).must_be :valid?
+        start_review_count = Review.count
+
+        post reviews_path, params: review_data
+
+        must_respond_with :redirect
+        must_redirect_to product_path(product.id)
+        Review.count.must_equal start_review_count + 1
+      end
+    end
+    # TODO: it "renders a bad_request when the review data is invalid" do
+      # what if the star is left blank...
   end
 end
