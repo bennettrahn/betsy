4ec3fdd1914cf6772eb7b18a81b4fb87f7e284d3
Andrea Chen <andreachen.ac@gmail.com>
2017-10-24 16:08:24 -0700

    Add test for PaymentInfo

--- app/models/payment_info.rb
+++ app/models/payment_info.rb
@@ -4,7 +4,7 @@ class PaymentInfo < ApplicationRecord
   validates :buyer_name, presence: true
   validates :email, presence: true
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Please enter your email in this format: example@example.com"
-  validates :card_number, presence: true #numericality
+  validates :card_number, presence: true, length: {minimum: 4} #numericality
   validates :expiration, presence: true
   # could be cool to format it like a date?
   validates :mailing_address, presence: true
